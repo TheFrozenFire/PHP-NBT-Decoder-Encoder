d246e7f7a1a69f36171fbe661fed344f857608d6
James Lowe <james_gh@jlowe.demon.co.uk>
2012-08-18 08:40:16 +0100

    nbt class: rework for TAG_LONG reads/writes
    *       Certain TAG_LONG reads/writes were incorrect on both 64-bit and
            32-bit PHP builds for differing reasons, mainly sign-related
            quirks of pack() and unpack().
    
    *       As I was fixing the above anyway, limited the GMP requirement
            to 32-bit PHP builds and added native 64-bit math to enable
            this, resulting in a speedup on 64-bit builds.
    
    Signed-off-by: James Lowe <james_gh@jlowe.demon.co.uk>

--- nbt.class.php
+++ nbt.class.php
@@ -10,10 +10,16 @@
  *  GMP Extension
  */
 
-extension_loaded("gmp") or trigger_error (
-	"The NBT class requires the GMP extension for 64-bit number handling. ".
-	"Execution will continue, but will halt if a 64-bit number is handled.", E_USER_NOTICE
-);
+if(PHP_INT_SIZE < 8) {
+	/*
+	 *  GMP isn't required for 64-bit machines as we're handling signed ints. We can use native math instead.
+	 *  We still need to use GMP for 32-bit builds of PHP though.
+	 */
+	extension_loaded("gmp") or trigger_error (
+		"The NBT class requires the GMP extension for 64-bit number handling on 32-bit PHP builds. ".
+		"Execution will continue, but will halt if a 64-bit number is handled.", E_USER_NOTICE
+	);
+}
 
 class NBT {
 	public $root = array();
@@ -111,15 +117,28 @@ class NBT {
 				if($unpacked >= pow(2, 31)) $unpacked -= pow(2, 32); // Convert unsigned int to signed int
 				return $unpacked;
 			case self::TAG_LONG: // Signed long (64 bit, big endian)
-				extension_loaded("gmp") or trigger_error (
-					"This file contains a 64-bit number and execution cannot continue. ".
-					"Please install the GMP extension for 64-bit number handling.", E_USER_ERROR
-				);
-				list(,$firstHalf) = unpack("N", fread($fp, 4));
-				list(,$secondHalf) = unpack("N", fread($fp, 4));
-				$value = gmp_add($secondHalf, gmp_mul($firstHalf, "4294967296"));
-				if(gmp_cmp($value, gmp_pow(2, 63)) >= 0) $value = gmp_sub($value, gmp_pow(2, 64));
-				return gmp_strval($value);
+				list(,$firstHalf, $secondHalf) = unpack("N*", fread($fp, 8));
+				if(PHP_INT_SIZE>=8) {
+					// Workaround for PHP bug #47564 in 64-bit PHP<=5.2.9
+					$firstHalf &= 0xFFFFFFFF; $secondHalf &= 0xFFFFFFFF;
+
+					$value = ($firstHalf << 32) | $secondHalf;
+					if($value>pow(2,63)) $value -= pow(2,64);
+				} else {
+					extension_loaded("gmp") or trigger_error (
+						"This file contains a 64-bit number and execution cannot continue. ".
+						"Please install the GMP extension for 64-bit number handling.", E_USER_ERROR
+					);
+
+					// Fix values >= 2^31 (same fix as above, but this time because it's > PHP_INT_MAX)
+					$firstHalf = gmp_and($firstHalf, "0xFFFFFFFF");
+					$secondHalf = gmp_and($secondHalf, "0xFFFFFFFF");
+
+					$value = gmp_add($secondHalf, gmp_mul($firstHalf, "4294967296"));
+					if(gmp_cmp($value, gmp_pow(2, 63)) >= 0) $value = gmp_sub($value, gmp_pow(2, 64));
+					$value=gmp_strval($value);
+				}
+				return $value;
 			case self::TAG_FLOAT: // Floating point value (32 bit, big endian, IEEE 754-2008)
 				list(,$value) = (pack('d', 1) == "\77\360\0\0\0\0\0\0")?unpack('f', fread($fp, 4)):unpack('f', strrev(fread($fp, 4)));
 				return $value;
@@ -166,13 +185,26 @@ class NBT {
 				if($value < 0) $value += pow(2, 32); // Convert signed int to unsigned int
 				return is_int(fwrite($fp, pack("N", $value)));
 			case self::TAG_LONG: // Signed long (64 bit, big endian)
-				extension_loaded("gmp") or trigger_error (
-					"This file contains a 64-bit number and execution cannot continue. ".
-					"Please install the GMP extension for 64-bit number handling.", E_USER_ERROR
-				);
-				$secondHalf = gmp_mod($value, 2147483647);
-				$firstHalf = gmp_sub($value, $secondHalf);
-				return is_int(fwrite($fp, pack("N", gmp_intval($firstHalf)))) && is_int(fwrite($fp, pack("N", gmp_intval($secondHalf))));
+				if(PHP_INT_SIZE>=8) {
+					$firstHalf = ($value & 0xFFFFFFFF00000000) >> 32;
+					$secondHalf = $value & 0xFFFFFFFF;
+
+					$wResult = is_int(fwrite($fp, pack("NN", $firstHalf, $secondHalf)));
+				} else {
+					extension_loaded("gmp") or trigger_error (
+						"This file contains a 64-bit number and execution cannot continue. ".
+						"Please install the GMP extension for 64-bit number handling.", E_USER_ERROR
+					);
+
+					// 32-bit values seem to be too long for pack() on 32-bit machines. Split into 4x16-bit instead.
+					$quarters[0] = gmp_div(gmp_and($value, "0xFFFF000000000000"), gmp_pow(2,48));
+					$quarters[1] = gmp_div(gmp_and($value, "0x0000FFFF00000000"), gmp_pow(2,32));
+					$quarters[2] = gmp_div(gmp_and($value, "0x00000000FFFF0000"), gmp_pow(2,16));
+					$quarters[3] = gmp_and($value, "0xFFFF");
+
+					$wResult = is_int(fwrite($fp, pack("nnnn", gmp_intval($quarters[0]), gmp_intval($quarters[1]), gmp_intval($quarters[2]), gmp_intval($quarters[3]))));
+				}
+				return $wResult;
 			case self::TAG_FLOAT: // Floating point value (32 bit, big endian, IEEE 754-2008)
 				return is_int(fwrite($fp, (pack('d', 1) == "\77\360\0\0\0\0\0\0")?pack('f', $value):strrev(pack('f', $value))));
 			case self::TAG_DOUBLE: // Double value (64 bit, big endian, IEEE 754-2008)
